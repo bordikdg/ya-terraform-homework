82da131392541e7643a439e2cdcc858e3032247a
bordikdg <bordikdg@gmail.com>
2024-08-07 11:16:20 +0000

    add tfvars file

--- variables.tf
+++ variables.tf
@@ -1,29 +1,23 @@
 variable "image_id" {
   description = "Disk image_for_vms_initialize_params_id"
-  default     = "fd83kelh78bs18a6cthf"
 }
 
 variable "folder_id" {
   description = "Folder id in Cloud"
-  default     = "b1gfcgssrdpu1otfatkf"
 }
 
 variable "service_account_id" {
   description = "Service account id"
-  default     = "aje54uo13hfuvg645ktf"
 }
 
 variable "cloud_id" {
   description = "Cloud id"
-  default     = "b1gi4qdhgc7fci8j1pfr"
 }
 
 variable "domain" {
   description = "Public DNS zone"
-  default     = "tf-homework.ru"
 }
 
 variable "dns_zone" {
   description = "Cloud dns"
-  default     = "tf-homework.ru."
 }
